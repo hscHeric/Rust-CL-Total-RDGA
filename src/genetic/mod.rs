93f202b52f52c22a3b962f5bd5ce65223c086ccf
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-12-30 00:01:00 -0300

    feat: add h1 heuristic for initial population generation

--- src/genetic/mod.rs
+++ src/genetic/mod.rs
@@ -1,11 +1,13 @@
 pub mod chromosome;
 pub mod crossover;
+pub mod heuristics;
 pub mod population;
 pub mod selection;
 
 pub use chromosome::Chromosome;
 pub use crossover::CrossoverStrategy;
 pub use crossover::TwoPointCrossover;
+pub use heuristics::h1;
 pub use population::Population;
 pub use selection::KTournamentSelection;
 pub use selection::SelectionStrategy;
