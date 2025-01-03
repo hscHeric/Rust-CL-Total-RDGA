5d4847dfb4619d26348587daa822c1b2bd05a3b5
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-01-03 17:56:40 -0300

    feat: enhance main to output CSV with trial results

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,120 @@
-use std::time::Instant;
+use std::process::exit;
+use std::{env, time::Instant};
 
+use cl_total_rdga::graph::parser::normalize_edges;
 use cl_total_rdga::{
-    genetic::{self, h1, CrossoverStrategy, SelectionStrategy},
-    graph,
+    genetic::{
+        h1, heuristics::h0, Chromosome, CrossoverStrategy, KTournamentSelection, Population,
+        SelectionStrategy, TwoPointCrossover,
+    },
+    graph::{parser::from_edge_list_file, SimpleGraph},
 };
-use genetic::{Chromosome, KTournamentSelection, Population, TwoPointCrossover};
-use graph::{SimpleGraph, SimpleGraphGenerator};
-use rand::Rng;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let vertex_count = rng.gen_range(0..10);
-    let edge_probability = rng.gen();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!(
+            "Uso: {} <file_path> <trials> [max_stagnant] [generations] [tournament_size] [crossover_prob] [pop_size]",
+            args[0]
+        );
+        exit(1);
+    }
 
-    let graph_generator = SimpleGraphGenerator::new(vertex_count, edge_probability);
-    let edges = match graph_generator.generate() {
-        Ok(edges) => edges,
-        Err(err) => {
-            eprintln!("Erro ao gerar o grafo: {}", err);
-            return;
+    // Parse dos argumentos obrigatórios
+    let file_path = &args[1];
+    let trials: usize = match args[2].parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Erro: 'trials' deve ser um número inteiro válido.");
+            exit(1);
         }
     };
 
-    let graph = match SimpleGraph::from_edges(edges) {
+    // Criação do grafo
+    let edge_list = from_edge_list_file(file_path);
+    let edge_list = normalize_edges(edge_list);
+    let graph = match SimpleGraph::from_edges(edge_list) {
         Ok(graph) => graph,
         Err(err) => {
             eprintln!("Erro ao criar o grafo: {:?}", err);
-            return;
+            exit(1);
         }
     };
 
-    println!(
-        "Grafo gerado com {} vértices e {} arestas.",
-        graph.vertex_count(),
-        graph.edge_count()
-    );
+    // Parse dos valores opcionais ( O padrão para os valores opcionais vai ser o mesmo do CL-RD)
+    let max_stagnant = args.get(3).and_then(|s| s.parse().ok()).unwrap_or(100);
+    let generations = args.get(4).and_then(|s| s.parse().ok()).unwrap_or(1000);
+    let tournament_size = args.get(5).and_then(|s| s.parse().ok()).unwrap_or(5);
+    let crossover_rate = args.get(6).and_then(|s| s.parse().ok()).unwrap_or(0.9);
+    let pop_size = args
+        .get(7)
+        .and_then(|s| s.parse().ok())
+        .unwrap_or_else(|| ((graph.vertex_count() as f64 / 1.5).ceil() as usize).max(1));
 
-    let population_size = 100;
-    let max_generations = 500;
-    let max_stagnant_generations = 50;
-    let tournament_size = 5;
-    let crossover_rate = 0.8;
-
-    let heuristics: Vec<fn(&SimpleGraph) -> Option<Chromosome>> = vec![h1];
-    let population = match Population::new(&graph, heuristics, population_size) {
-        Ok(population) => population,
-        Err(err) => {
-            eprintln!("Erro ao criar a população inicial: {:?}", err);
-            return;
-        }
-    };
-
-    for individual in population.individuals() {
-        println!("individual_fitness = {}", individual.fitness())
-    }
-
+    let heuristics: Vec<fn(&SimpleGraph) -> Option<Chromosome>> = vec![h1, h0];
     let selection_strategy = KTournamentSelection { tournament_size };
     let crossover_strategy = TwoPointCrossover { crossover_rate };
 
-    let start_time = Instant::now();
+    println!("graph_name,graph_order,graph_size,fitness_value,elapsed_time(microsecond)");
+    for trial in 0..trials {
+        let start_time = Instant::now();
+        let mut population = match Population::new(&graph, heuristics.clone(), pop_size) {
+            Ok(population) => population,
+            Err(err) => {
+                eprintln!("Erro ao criar a população inicial: {:?}", err);
+                return;
+            }
+        };
 
-    let mut best_solution = population.best_individual().unwrap();
-    for generation in 0..max_generations {
-        let selected_population = selection_strategy.select(&population);
+        let mut best_solution = match population.best_individual() {
+            Ok(individual) => individual,
+            Err(err) => {
+                eprintln!(
+                    "Erro ao obter o melhor indivíduo inicial no trial {}: {:?}",
+                    trial, err
+                );
+                continue;
+            }
+        };
 
-        let offspring_population = crossover_strategy.crossover(&selected_population, &graph);
+        let mut stagnant_generations = 0;
+        for generation in 0..generations {
+            let selected_population = selection_strategy.select(&population);
+            let offspring_population = crossover_strategy.crossover(&selected_population, &graph);
+            population = offspring_population.validate_population(&graph);
 
-        let new_best_solution = offspring_population.best_individual().unwrap();
-        if new_best_solution.fitness() < best_solution.fitness() {
-            best_solution = new_best_solution;
+            let new_best_solution = match population.best_individual() {
+                Ok(individual) => individual,
+                Err(err) => {
+                    eprintln!(
+                        "Erro ao obter o melhor indivíduo na geração {} do trial {}: {:?}",
+                        generation, trial, err
+                    );
+                    break;
+                }
+            };
+
+            if new_best_solution.fitness() < best_solution.fitness() {
+                best_solution = new_best_solution;
+                stagnant_generations = 0;
+            } else {
+                stagnant_generations += 1;
+            }
+
+            if stagnant_generations >= max_stagnant {
+                break;
+            }
         }
-
-        if generation >= max_stagnant_generations {
-            break;
-        }
+        let elapsed_time = start_time.elapsed();
+        let graph_name = file_path.split('/').last().unwrap_or("unknown");
 
         println!(
-            "Geração {}: Melhor fitness = {}",
-            generation,
+            "{},{},{},{},{}",
+            graph_name,
+            graph.vertex_count(),
+            graph.edge_count(),
             best_solution.fitness(),
+            elapsed_time.as_micros()
         );
     }
-
-    let elapsed_time = start_time.elapsed();
-    println!("Tempo total de execução: {:.2?}", elapsed_time);
-    println!(
-        "Melhor solução encontrada: Fitness = {}",
-        best_solution.fitness(),
-    );
 }
